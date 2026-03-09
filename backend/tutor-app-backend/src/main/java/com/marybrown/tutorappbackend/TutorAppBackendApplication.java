5c4525cf28d54b0be9c0dea88aa06f97d251eca3
Maria Biss <mariabiss3012@gmail.com>
2026-03-09 12:11:22 +0100

    init spring boot backend with postgres and docker

---
+++ backend/tutor-app-backend/src/main/java/com/marybrown/tutorappbackend/TutorAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.marybrown.tutorappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorAppBackendApplication.class, args);
+    }
+
+}
---
+++ backend/tutor-app-backend/src/test/java/com/marybrown/tutorappbackend/TutorAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marybrown.tutorappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
